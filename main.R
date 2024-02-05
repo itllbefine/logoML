af161161bfc28e98c723b1a85c59128d73c38e1a
itllbefine <goncus@gmail.com>
2024-02-05 16:56:49 -0500

    Adding 'main.R' and testing version control on new setup.

---
+++ main.R
@@ -0,0 +1,3 @@
+### The purpose of this code is to learn how to apply machine learning to pictures.
+### I have a hypothesis that a company's logo is sufficient to ascertain its industry with 90+% accuracy.
+
